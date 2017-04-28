2943c2ea2a0d66d5abebeebb5c11cce374af26fa
tjnolan24 <tjnolan@rams.colostate.edu>
2017-04-28 16:48:35 -0600

    Update ajax_listing.php

--- Index/ajax_listing.php
+++ Index/ajax_listing.php
@@ -1,1 +1,25 @@
-l
+l<?php 
+	header ( 'Content-Type: text/json' );
+	header ( "Access-Control-Allow-Origin: *" );
+?>
+
+<?php 
+	//display ingredients in database
+	require_once '../lib/Database.php';
+	$db = new Database();
+	$ingredients = $db->getIngredients();
+	
+	$json = array(); //the json object that will return all ingredients
+	
+	foreach($ingredients as $ing){
+		$name = $ing["ingredient_name"];
+		$short = $ing["short"];
+		$unit = $ing["unit"];
+		$cost = $ing["price"];
+		$temp = array("name" => $name, "short" => $short, "unit" => $unit, "cost" => $cost);
+		$json[] = $temp;
+	}
+	
+	echo json_encode($json);
+	
+?>
