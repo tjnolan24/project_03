93a24445fd4b75247287bc6b325d361f54765f0e
tjnolan24 <tjnolan@rams.colostate.edu>
2017-04-28 16:29:32 -0600

    Update test_ajax.php

--- Index/test_ajax.php
+++ Index/test_ajax.php
@@ -3,6 +3,7 @@
 <?php  
     include '../Login/Control.php';
    $pageTitle = 'Test Ajax';
+   include_once './Create.php';
    include '../Header and Footer/Header.php';
 ?>
 
@@ -13,12 +14,15 @@
 <script>
 	$(document).ready(function(){
 		var http = new XMLHttpRequest();
-		http.open("GET", "ajax_status.php", true);
+		http.open("GET", "ajax_ingrimage.php?ing=Wasabi", true);
 		
 		http.onreadystatechange = function(){
 			if (this.readyState == 4 && this.status == 200){
 				var myObj = JSON.parse(this.responseText);
-				$("#test").text(myObj.status);
+				$("#test").text(this.responseText);
+				
+				//use in ajax_ingrimage
+				$("#rand_img").attr("src", 'data:image/png;base64,' + myObj[0]); //change src of image
 			}
 		}
 		http.send(null);
@@ -26,7 +30,9 @@
 </script>
 
 <div class="container-fluid" id="content">
-	<p align="center" id="test">This Text will change if it gets the JSON object successfully from ajax_status.php</p>
+	<p align="center" id="test">This Text will change if it gets the JSON object successfully from ajax_pages</p><br/><br/><br/>
+	<p align="center">The following image will appear if ajax_ingrimage worked</p>
+	<img id="rand_img" src="" alt="Random Image">
 </div>
 
 <?php include "../Header and Footer/Footer.php"; ?>
