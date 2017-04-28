e612698a7445e2304f6d5fcd42d7f70f5b88eee2
tjnolan24 <tjnolan@rams.colostate.edu>
2017-04-28 15:03:47 -0600

    Update ajax_ingredient.php

--- Index/ajax_ingredient.php
+++ Index/ajax_ingredient.php
@@ -1,1 +1,31 @@
-k
+<?php 
+	header ( 'Content-Type: text/json' );
+	header ( "Access-Control-Allow-Origin: *" );
+?>
+
+
+<?php 
+	//display ingredients in database
+	require_once '../lib/Database.php';
+	$db = new Database();
+	
+	$ing_name = "";
+	
+	if (isset($_GET['ing'])){
+		
+		$ing_name = $_GET['ing'];
+		$ing = $db->retrieveIngredient($ing_name);
+		
+		$name = $ing["ingredient_name"];
+		$short = $ing["short"];
+		$unit = $ing["unit"];
+		$cost = $ing["price"];
+		$time = $ing["time"];
+		$desc = $ing["description"];
+		
+		$temp = array("name" => $name, "short" => $short, "unit" => $unit, "cost" => $cost, "time" => $time, "desc" => $desc);
+		echo json_encode($temp);
+		
+	}
+		
+?>
