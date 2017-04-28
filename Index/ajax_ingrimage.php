ea6f5bd34d27edd7d02fb37c30dd2b650a485247
tjnolan24 <tjnolan@rams.colostate.edu>
2017-04-28 16:28:42 -0600

    Update ajax_ingrimage.php

--- Index/ajax_ingrimage.php
+++ Index/ajax_ingrimage.php
@@ -1,1 +1,27 @@
-k
+<?php 
+	header ( 'Content-Type: text/json' );
+	header ( "Access-Control-Allow-Origin: *" );
+?>
+
+
+<?php 
+	//display ingredients in database
+	require_once '../lib/Database.php';
+	$db = new Database();
+	
+	$ing_name = "";
+	
+	if (isset($_GET['ing'])){
+		
+		$ing_name = $_GET['ing'];
+		$ing = $db->retrieveIngredient($ing_name);
+		
+		$img = $ing["image"];
+		$encoded_data = base64_encode(file_get_contents("../Ingredient Pages/$img")); 
+		
+		$temp = array($encoded_data);
+		echo json_encode($temp);
+		
+	}
+		
+?>
